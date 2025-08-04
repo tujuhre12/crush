f59b2511868d1f076f68f9764b181d27a13b4f03
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-08-04 15:22:33 -0300

    fix: small improvements on auto-updater

--- internal/cmd/update.go
+++ internal/cmd/update.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/charmbracelet/crush/internal/format"
 	"github.com/charmbracelet/crush/internal/update"
 	"github.com/spf13/cobra"
 )
@@ -20,15 +23,21 @@ var updateCmd = &cobra.Command{
 crush check-update
   `,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Checking for updates...")
+		ctx, cancel := context.WithTimeout(cmd.Context(), 10*time.Second)
+		defer cancel()
 
-		info, err := update.CheckForUpdate(cmd.Context())
+		sp := format.NewSpinner(ctx, cancel, "Checking for updates")
+		sp.Start()
+		defer sp.Stop()
+
+		info, err := update.CheckForUpdate(ctx)
 		if err != nil {
 			return fmt.Errorf("failed to check for updates: %w", err)
 		}
+		sp.Stop()
 
 		if !info.Available {
-			fmt.Printf("You are running the latest version (%s)\n", info.CurrentVersion)
+			fmt.Printf("You are running the latest version: %s\n", info.CurrentVersion)
 			return nil
 		}
 
